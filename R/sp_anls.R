52f1331b8f5aa4fd23fbebacef95a0ba8b35bdf7
unknown <nikolaos.tziolas@wur.nl>
2015-01-12 09:53:59 +0100

    add comments

--- R/sp_anls.R
+++ R/sp_anls.R
@@ -2,8 +2,9 @@
 # Date: 8 January 2015
 # Dependencies: sp, rgdal, rgeos
 
-# The Function perform a simple spatial analysis for two input shapefiles.
-## Particularly the user specify the type and select the width to create a buffer around them, then an intersection is performed with the buffer and the shpA. The output is a a plot that shows the buffer, the points, and the name of the city such as a text with some comments about the city
+# the Function perform a simple spatial analysis for two input shapefiles 
+# the user can perform an intersection between the inputs 
+# in this example, we use places and railways as shapefiles input. 
 
 # load libraries
 library(sp)
@@ -11,29 +12,34 @@ library(rgdal)
 library(rgeos)
 
 sp_anls<- function(places, railways, type, width) { 
+  # @param places is a shapefile
+  # @param railways is a shapefile
+  # type is the object inside shapefile where we can perform a buffer
+  # width is the buffer width
+  
   places <- readOGR("data/url", places)
   railways <- readOGR("data/url", railways)
   
-  # transform to the selected projection system
-  places_proj<-spTransform(places, prj_string_RD)
-  railways_proj<-spTransform(railways, prj_string_RD)
+# transform to the selected projection system
+places_proj<-spTransform(places, prj_string_RD)
+railways_proj<-spTransform(railways, prj_string_RD)
   
-  # make a subset according to the type that is specified by the user, input should be within "".
-  subset <- railways_proj[railways_proj$type == type, ]
+# make a subset according to the type that is specified by the user, input should be within "".
+subset <- railways_proj[railways_proj$type == type, ] # type is one of fieldname in the railways shapefile
   
-  # create a buffer zone accordinf to the width that is specified by the user
-  buffer<- gBuffer(subset, width= width, byid= TRUE)
+# perform a buffer zone for the specified subset
+buffer<- gBuffer(subset, width= width, byid= TRUE)
   
-  # make intersect
-  a<- gIntersection(buffer,places_proj, byid=TRUE)
-  b<- gIntersects(buffer,places_proj, byid=TRUE)
+# perform an intersection between the buffer object from the 2nd input with an    object in the 1st input that we want to define
+a<- gIntersection(buffer,places_proj, byid=TRUE)
+b<- gIntersects(buffer,places_proj, byid=TRUE)
  
-  # select the city name
-  selected<-places_proj@data[b]
+# select the name of the object which intersect with the buffered object
+selected<-places_proj@data[b]
   
-  #plot the final outputs
-  plot(buffer,col='lightblue' )
-  points(a, col="red", pch=22)
-  mtext(side = 3, line = 1, paste(selected[2]), cex = 0.8)
-  text<-print(paste("The name of the city is", selected[2], "and the population is equal to",selected[4])) 
+#plot the final outputs
+plot(buffer,col='lightblue' )
+points(a, col="red", pch=22)
+mtext(side = 3, line = 1, paste(selected[2]), cex = 0.8)
+text<-print(paste("The name of the city is", selected[2], "and the population is equal to",selected[4])) 
 } 
--- main.R
+++ main.R
@@ -26,4 +26,5 @@ lapply(URLs, unzipURL)
 prj_string_RD <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +units=m +no_defs")
 
 # spatial analysis
+# In this example we want to define which places are intersect with railways when we perform a buffer of 1000m width to industrial railways 
 sp_anls("places", "railways", "industrial", 1000)
